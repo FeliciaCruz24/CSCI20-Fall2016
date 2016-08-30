4d641ed7e13b683cadd2da684abd07369dfe228a
Felicia Cruz <fcruz001@student.butte.edu>
2016-08-30 05:13:32 +0000

    lab1 folder update for lab1.cpp

---
+++ lab1/lab1.cpp
@@ -0,0 +1,16 @@
+/*Felicia Cruz 
+August 25th 2016
+This program is a picture of my initials.
+*/
+
+# include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "FFFFF"<<"   "<< "CCCCC"<<endl;
+    cout<< "F"<< "     "<< "  C  "<<endl;
+    cout<<"FFFFF"<< "   "<< "C  "<<endl;
+    cout<<"F"<< "     "<< "  C  "<<endl;
+    cout<<"F"<< "   "<< " O"<<"  CCCCC"<< endl;
+
+}  
