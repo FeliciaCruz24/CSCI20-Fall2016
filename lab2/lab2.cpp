5b32c5ba35ef6a9915d943c19afbfeb68f2cdf07
Felicia Cruz <fcruz001@student.butte.edu>
2016-08-30 06:06:58 +0000

    lab2 folder updated for lab2.cpp

---
+++ lab2/lab2.cpp
@@ -0,0 +1,13 @@
+/* Felicia Cruz
+August 28th 2016
+This is an algorithm that is for a number guessing game on the computer.
+*/
+
+// The computer will choose between 2 numbers to randomly output.
+//The numbers are 4 and 9. Variables will be randomNum1 and randomNum2.
+//At the top of the screen the computer will say "Guess a number between 1 and 10".
+//Underneath there will be a text box where the user types thir guess.
+// User types their guess and if the number is lower the program will say "lower" or if the number is higher it will say "higher".
+//If the user guesses correctly they will win ten points.
+// If they do not guess the right number they will lose one point starting from 10 till they get the right answer. Variable will be int losePoint = 10; The equation wll be: losePoint= losePoint -1; everytime the 
+// They will continue to guess till the get the right answer.
