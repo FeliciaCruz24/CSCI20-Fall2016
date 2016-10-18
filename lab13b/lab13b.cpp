9db464ed24988ad1be8c838e31eff9e6b9b6d14b
Felicia Cruz <fcruz001@student.butte.edu>
2016-10-18 00:24:09 +0000

    lab13b submission

--- lab13b/lab13b.cpp
+++ lab13b/lab13b.cpp
@@ -1,5 +1,5 @@
 /*Felicia Cruz
-October 11th 2016
+October 17th 2016
 This program is going to print every minute between 1:00 pm and 2:59 pm and accept user input of 2 different times and print every 15 minute interverals.
 */
 
@@ -9,26 +9,24 @@ using namespace std;
 
 int main(){
     //1st hour
-    int time0Minutes = 0;//0 min
-    int time59Minutes = 0;//59 min
+    float time0Minutes = 0;//0 min
+    float time59Minutes = 0;//59 min
     //2nd hour
     int time_0_Minutes =0;//0 min
     int time_59_Minutes =0;//59 min
     char oneHour ='1';
-    char twoHour='2';
+    char twoHour ='2';
     char timeGap =':';
     int startHour = 0;//The start time the user inputs
     int endHour = 0;//The end time the user inputs
     int startMinutes =0;
-    int endMinutes = 0;
+    int endMinutes =0;
     
-    time0Minutes = 0;
-    time59Minutes = 59;
-    time_0_Minutes =0;
-    time_59_Minutes =59;
+   
+    time59Minutes = 60;
+    time_59_Minutes =60;
     
-     while(time0Minutes<time59Minutes){//The time from 1:00-1:59pm
-        time0Minutes++;
+    for(time0Minutes= 0; time0Minutes<time59Minutes;time0Minutes++){//The time from 1:00-1:59pm
         if(time0Minutes<10){
                 cout<<oneHour<<timeGap<<"0"<<time0Minutes<<"pm"<<endl;
             }
@@ -36,24 +34,25 @@ int main(){
                 cout<<oneHour<<timeGap<<time0Minutes<<"pm"<<endl;
             }
         }
-     while(time_0_Minutes<time_59_Minutes){//The time from 2:00 -2:59pm
-        time_0_Minutes++;
-         if(time_0_Minutes<10){
+     for(time_0_Minutes=0; time_0_Minutes<time_59_Minutes; time_0_Minutes++){//The time from 2:00 -2:59pm
+        if(time_0_Minutes<10){
                 cout<<twoHour<<timeGap<<"0"<<time_0_Minutes<<"pm"<<endl;
             }
             else{
                 cout<<twoHour<<timeGap<<time_0_Minutes<<"pm"<<endl;
             }
-        }
-    cout<<" "<<endl;
+     }
     cout<<"Enter a start hour: ";//the user enters the start hour
-    cin>>startHour;
+    cin>>startHour>>timeGap>>startMinutes;
     cout<<"Enter end hour: ";//the user enters the end hour
-    cin>>endHour;
-    cout<<"FIX THE LOOP";
-
-    for(startHour; startHour<endHour; startHour + 15){//this displays the time every fifteen minutes
-        cout<<startHour<<"pm"<<endl;
+    cin>>endHour>>timeGap>>endMinutes;
+    cout<<"FIX THIS LOOP";
+    /*
+     for(startMinutes; startMinutes<60; startMinutes + 15){//this displays the time every fifteen minutes
+        cout<<startMinutes<<"pm"<<endl;
+         if(startMinutes >=60){
+            startMinutes = endMinutes;
+         }
     }
-    
+    */
 }
